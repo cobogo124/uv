c7d09b7aedb93648e5f9f4fa19e9825a21ee6f78
cobogo124 <conorbronn05@gmail.com>
2025-02-16 18:12:42 +0000

    Add files via upload

--- main_node.py
+++ main_node.py
@@ -8,48 +8,118 @@ from geometry_msgs.msg import Twist, Point
 from uvcontroller.msg import Sector
 from std_msgs.msg import Bool
 
+
 rospy.init_node("main_node")
 
-initialx, initialy = 1, 1
+goal = (1,1)
 initialReached = False
 rate = rospy.Rate(10)
+toleratedDistance = 0.2
 command = Twist()
-
+humanSector = Sector()
+path = []
 currentPosition = Pose()
 
 sec_pub = rospy.Publisher("/human/sector", Sector, queue_size=10)
 vel = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
 
-def get_square_coordinates(x, y):
-    square_x = int(x // 2)
-    square_y = int(y // 2)
-    return (square_x, square_y)
+def getHumanSector(current: Sector):
+    global humanSector
+    humanSector = current
+
+def euclideanDistance() :
+    return math.sqrt(pow((goal[0]-currentPosition.x), 2) + pow((goal[1]-currentPosition.y), 2))
 
 def getLocation(current: Pose):
     global currentPosition
-    currentPosition = (current.x, current.y, current.theta)
+    currentPosition = current
+
+def pathFindRest(startingx, startingy, trueForRight):
+    goRight = trueForRight
+    currentx = startingx
+    currenty = startingy
+    while currentx != 4 and currenty != 4:
+        if goRight:
+            for i in range(currentx, 5):
+                sector = Sector()
+                sector.x = i
+                sector.y = currenty
+                path.append(sector)
+                currentx = i
+            currenty += 1
+            goRight = not goRight
+        else:
+            for i in reversed(range(currentx)):
+                sector = Sector()
+                sector.x = i
+                sector.y = currenty
+                path.append(sector)
+                currentx = i
+            currenty += 1
+            goRight = not goRight
+
+def pathFindUpTo(endx, endy):
+    goRight = True
+    currentx = 0
+    currenty = 0
+    while currentx != endx and currenty != endy:
+        if goRight:
+            for i in range(5):
+                sector = Sector()
+                sector.x = i
+                sectory = currenty
+                path.append(sector)
+                currentx = i
+            sectory += 1
+            goRight = not goRight
+        else:
+            for i in reversed(range(currentx)):
+                sector = Sector()
+                sector.x = i
+                sector.y = currenty
+                path.append(sector)
+                currentx = i
+            currenty += 1
+            goRight = not goRight
+
+def pathFindForHuman():
+    minx, miny = 0
+    maxx, maxy = 4
+    currentx = 0
+    currenty = 0
+    if humanSector.x == maxx:
+        if humanSector.y == miny:
+            pathFindUpTo(3, 0)
+            for i in range(humanSector.x - 1):
+                sector = Sector()
+                sector.x = i
+                sector.y = currenty
+                path.append(sector)
+            sector = Sector()
+            sector.x =
 
 rospy.Subscriber("/turtle1/pose", Pose, getLocation)
+rospy.Subscriber("/human/sector", Sector, getHumanSector)
 
-while initialReached == False:
-    diff = (1 - currentPosition[0], 1 - currentPosition[1])
-    goal_orientation = math.atan2(diff[1], diff[0])
-
-    angle_diff = angles.shortest_angular_distance(currentPosition[2], goal_orientation)
-
-    if angle_diff < -0.087 or angle_diff > 0.087:
-        command.angular.z = -1
+while not initialReached:
+    if euclideanDistance() <= 0.2:
+        initialReached = True
+        command.angular.z = 0
         command.linear.x = 0
     else:
-        command.angular.z = 0
-        command.linear.x = 3
+        diff = (goal[0] - currentPosition.x, goal[1] - currentPosition.y)
+        goal_orientation = math.atan2(diff[1], diff[0])
+        angle_diff = angles.shortest_angular_distance(currentPosition.theta, goal_orientation)
+
+        if angle_diff < -0.087 or angle_diff > 0.087:
+            command.angular.z = -1
+            command.linear.x = 0
+        else:
+            command.angular.z = 0
+            command.linear.x = 3
 
     vel.publish(command)
     rate.sleep()
 
-while not rospy.is_shutdown():
-    if not initialReached:
-        goToInitial()
-    else:
-        print("not yet")
-    rate.sleep()
+# while not rospy.is_shutdown():
+#     pathFind()
